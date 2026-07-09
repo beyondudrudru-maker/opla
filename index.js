e1e40be92bc3b41900250df210056f44f8c3f14f
beyondudrudru-maker <beyondudrudru@gmail.com>
2026-07-10 00:23:24 +0530

    index.js

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent 
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessageReactions // 👈 YE MISSING THA!
     ]
 });
 
